198d75ff2640026e1d12b0cd71d8eb394d437b54
Punch1n3ll0 <corsoking@icloud.com>
2024-06-21 15:06:44 -0400

    Completed exercise 5 demonstrating ability to create sumAll() and test the given exercise constraints with Jest

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (n1 < 0 || n2 < 0) return "ERROR";
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') return "ERROR";
+    let lowerBound = Math.min(n1, n2);
+    let upperBound = Math.max(n1, n2);
+    let sum = 0;
 
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+    return sum; 
 };
 
 // Do not edit below this line
