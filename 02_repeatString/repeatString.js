0a7381c0f9559a89d06a2518725ceb9ffe515731
Punch1n3ll0 <corsoking@icloud.com>
2024-08-21 11:46:41 -0400

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+function repeatString(str, num) {
+    // Input validation
+    if (typeof str !== 'string') {
+      return "ERROR";
+    }
 
-};
+    if (typeof num !== 'number' || num < 0 || !Number.isInteger(num)) {
+        return "ERROR"
+    }
 
+    return str.repeat(num);
+}
 // Do not edit below this line
 module.exports = repeatString;
