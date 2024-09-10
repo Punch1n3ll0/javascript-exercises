a82dc7dd069f53897d4f541cde99d52c6787b674
Punch1n3ll0 <corsoking@icloud.com>
2024-09-10 12:26:41 -0400

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function(arr) {
+	if (arr.length === 0) return 0;
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+};
+
+const multiply = function(numbers) {
+  return numbers.reduce((product, number) => product * number, 1);
+};
+
+const power = function(num, pow) {
+  return Math.pow(num, pow);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+	return num * factorial(num -1);
 };
 
 // Do not edit below this line
